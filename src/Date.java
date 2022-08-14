05c2c819857e822948549dc80f4765da9f1c86bc
Piso00 <s423075>
2022-08-15 00:01:21 +0200

    First commit

---
+++ src/Date.java
@@ -0,0 +1,12 @@
+public class Date {
+    int newYear;
+    int newMonth;
+    int newDay;
+
+    //constructor
+    public Date(int year, int month, int day){
+        newYear = year;
+        newMonth = month;
+        newDay = day;
+    }
+}
---
+++ src/Event.java
@@ -0,0 +1,8 @@
+public class Event {
+    String event_name;
+    Date event_date;
+
+
+}
+
+
